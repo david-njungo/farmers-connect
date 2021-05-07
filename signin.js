5015a4cfdc86882ba23fa0de6a824d78aceaadc0
davidnjungo-lab <david.kimani@student.moringaschool.com>
2021-05-07 08:41:29 +0300

    add link

--- signin.js
+++ signin.js
@@ -3,9 +3,9 @@ function login() {
     var password = document.getElementById("password").value;
 
     if (form.username.value == "myusername" && form.password.value == "mypassword") {
-        window.open("index.html")
+        window.open("index.html");
     } else {
-        alert("Error wrong Password or Username")
+        alert("Error wrong Password or Username");
     }
 
 
